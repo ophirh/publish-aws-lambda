cc0bc05acf6cac229a9b2e9ff6edc449025889e3
Ophir <ophir@itculate.io>
2015-12-24 12:13:50 -0500

    Implicitly add this pacakge to requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='publish-aws-lambda',
-    version='0.1.1',
+    version='0.2',
     description='Publish a Python module as a set of AWS lambda functions',
     url='https://github.com/ophirh/publish-aws-lambda',
     author='Ophir',
