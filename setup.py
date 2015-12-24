9cb532115f44b25101efa7d448328e35ca51c37c
Ophir <ophir@itculate.io>
2015-12-24 16:15:48 -0500

    Fix issue with deleting boto3 from ZIP file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='publish-aws-lambda',
-    version='0.3',
+    version='0.3.1',
     description='Publish a Python module as a set of AWS lambda functions',
     url='https://github.com/ophirh/publish-aws-lambda',
     author='Ophir',
