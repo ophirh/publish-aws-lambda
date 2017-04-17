09667eda2dadb2603f8629fd8617e0da9434cca3
Ophir <ophir@itculate.io>
2017-04-17 13:40:12 -0400

    Add support for VPC config in Lambda

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='publish-aws-lambda',
-    version='0.3.3',
-    description='Publish a Python module as a set of AWS lambda functions',
-    url='https://github.com/ophirh/publish-aws-lambda',
-    author='Ophir',
-    author_email='opensource@itculate.io',
-    license='MIT',
-    keywords=['aws', 'lambda', 'publish'],
+    name="publish-aws-lambda",
+    version="0.4.0",
+    description="Publish a Python module as a set of AWS lambda functions",
+    url="https://github.com/ophirh/publish-aws-lambda",
+    author="Ophir",
+    author_email="opensource@itculate.io",
+    license="MIT",
+    keywords=["aws", "lambda", "publish"],
     packages=find_packages(),
 )
